23cdafcf00827d9a2ee104a37d66f0a9330e242e
Wilamaze <orlando.aguilar@live.com>
2023-04-30 14:55:06 -0400

    change the style, fixed the checkboxes to work

--- client/script.js
+++ client/script.js
@@ -1,58 +1,73 @@
-// sending a get request to spacex api
+// Defining a variable and setting it to null in order to delete chart.
+let myChart = null;
+
+// deleteChart function in order to check if chart exists and delets the instance of it. 
+function deleteChart() {
+  if (myChart) {
+    myChart.destroy();
+  }
+}
+
+// sending a request to spacex api
 async function getData() {
-    try {
-      const response = await fetch('https://api.spacexdata.com/v4/launches');
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.log(error);
-    }
+    const response = await fetch('https://api.spacexdata.com/v4/launches');
+    const data = await response.json();
+    return data;
   }
-  
-  // Creating pie chart function
-  async function createChart() {
-    try {
-      const data = await getData();
-      let successCount = 0;
-      let failureCount = 0;
-      data.forEach(mission => {
-        if (mission.success) {
-          successCount++;
-        } else {
-          failureCount++;
-        }
-      });
 
-      // Creating the pie chart data object
-      const chartData = {
-        labels: ['Successful Mission', 'Mission Failure'],
-        datasets: [{
-            // data is the successful and failing missions 
-          data: [successCount, failureCount],
-          backgroundColor: ['#9BB39C', '#F67B7B']
-        }]
-      };
+// creating the chart to display data 
+async function createChart() {
+  const data = await getData();
 
-      // Creating Pie Chart configurating object
-      const pieConfig = {
-        type: 'pie',
-        data: chartData,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'SpaceX Launch Success/Failure Chart'
-            }
-          }
-        }
-      };
-      new Chart(document.getElementById('myChart'), pieConfig);
-      // log errors to console.
-    } catch (error) {
-      console.log(error);
+  // defining the count variables for missions
+  let successCount = 0;
+  let failureCount = 0;
+
+  const successCheckbox = document.getElementsByName("success")[0];
+  const failureCheckbox = document.getElementsByName("failure")[0];
+  const showSuccess = successCheckbox.checked;
+  const showFailure = failureCheckbox.checked;
+
+  // check through the data of the failure and successful missions and counting each to show data.
+  data.forEach(mission => {
+    if (mission.success) {
+      if (showSuccess) {
+        successCount++;
+      }
+    } else {
+      if (showFailure) {
+        failureCount++;
+      }
     }
+  });
+
+  // creating the pie chart data object
+  const chartData = {
+    labels: ['Successful Mission', 'Mission Failure'],
+    datasets: [{
+      data: [successCount, failureCount],
+      backgroundColor: ['#9BB39C', '#F67B7B']
+    }]
+  };
+
+  // configuring the pie chart object
+  const pieConfig = {
+    type: 'pie',
+    data: chartData,
+    options: {
+      responsive: false,
+      plugins: {
+        legend: { position: 'top' },
+        title: { display: true, text: 'SpaceX Launch Success/Failure Chart' }
+      }
+    }
+  };
+
+  // if deleteChart doesn't work, this will check for it again and delete it.
+  if (myChart !== null) {
+    myChart.destroy();
   }
+
+  // Creating aa new chart object and assign it to 'myChart'
+  myChart = new Chart(document.getElementById('myChart'), pieConfig);
+}
